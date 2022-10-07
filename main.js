56204ead6aca0b809c7bbba66293ad39f83e469b
Jesier <jesiersantiago@gmail.com>
2022-10-07 10:40:47 -0500

    push

---
+++ main.js
@@ -0,0 +1,12 @@
+const add = (firstNumber,secondNumber) =>{
+   const added = firstNumber + secondNumber
+        return added
+}
+
+const multiplyByTwo = (number) => {
+    const multiplied = number * 2
+        return multiplied
+}
+add(2,3)
+const twoPlusThree = 5
+console.log(multiplyByTwo(twoPlusThree))
